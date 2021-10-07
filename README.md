4d945f2eb875a4c34f52f39446ecf1a243479313
Bantu Sotyato <sotyatobantu@gmail.com>
2021-10-07 12:31:53 +0200

    made moren changes on the handlebars file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# overgear-manager
+# Overgeared Restaurant's Employee Management System
+Employee Management System is a Java application, developed to maintain the shift details of employees working in the Overgeared Restaurant. It maintains information about how many shifts each waiter should work and the days they will be on standby. The application is completed developed using Java Language, Spark, Maven and JDbi SQL Library.
+
+OBJECTIVE OF THE PROJECT
+------------------------ 
+
+Due to the Overgeared Restaurant wanting to implement effiecincy in their waiter shift management they wanted to create this project. This project simplifies the task of maintaining records because of its user-friendly nature.
+
+Features:
+1. Login
+2. Create waiter details as a new employee.
+3. Add, Remove, Update and print the details of a waiter shift roster.
+4. Managing days to see if they ha enough, too few or too many waiters working.
+5. Manager able to accept or reject waiter requests for absence and work shift change to work on a day.
+6. Waiter able to submit absence reports and suggest days to be absent.
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import spark.template.velocity.VelocityTemplateEngine;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,29 +15,29 @@ public class App {
     public static void main(String[] args) {
         Manager manager = new Manager();
         List<String> weekdays = new ArrayList<>();
-manager.createTables();
+        manager.createTables();
         weekdays.add("Monday");
         weekdays.add("Tuesday");
-      manager.updateWaiterShift("tom",weekdays);
+//      manager.updateWaiterShift("tom",weekdays);
 //        System.out.println(weekdays);
         System.out.println(manager.waiterNames());
         port(2000);
         staticFiles.location("/public");
+        get("/", (req, res) -> {
+            Map<String, Object> map = new HashMap<>();
+            return new ModelAndView(map, "landingPage.handlebars");
+        }, new HandlebarsTemplateEngine());
+
          get("/roster", (req, res) -> {
             Map<String, Object> map = new HashMap<>();
             return new ModelAndView(map, "roster.handlebars");
         }, new HandlebarsTemplateEngine());
 
-        get("/login/roster", (req, res) -> {
+        get("/rosters", (req, res) -> {
             Map<String, Object> map = new HashMap<>();
-            return new ModelAndView(map, "roster.handlebars");
+            return new ModelAndView(map, "rosters.handlebars");
         }, new HandlebarsTemplateEngine());
 
-
-        get("/login", (req, res) -> {
-            Map<String, Object> map = new HashMap<>();
-            return new ModelAndView(map, "login.handlebars");
-        }, new HandlebarsTemplateEngine());
 
 //        get("/roster", (req, res) -> {
 //            Map<String, Object> map = new HashMap<>();

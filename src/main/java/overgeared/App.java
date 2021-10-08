ffbaf5309563353d6a210ec8cc870c4b8572381f
Bantu Sotyato <sotyatobantu@gmail.com>
2021-10-08 11:34:15 +0200

    fixed deploy files

--- src/main/java/overgeared/App.java
+++ src/main/java/overgeared/App.java
@@ -13,7 +13,19 @@ import java.sql.*;
 import static spark.Spark.*;
 
 public class App {
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
         Manager manager = new Manager();
 //        List<String> weekdays = new ArrayList<>();
         manager.createTables();
